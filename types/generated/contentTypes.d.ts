7ae2ced9b3fdaf5c00c607294545a51ca064ac77
Francisco Braga <franciscobragafernandes@gmail.com>
2026-03-01 11:38:03 -0300

    Change portuguese_translation column to long text

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -534,7 +534,7 @@ export interface ApiPratisakhyaSutraPratisakhyaSutra
       Schema.Attribute.Private;
     number: Schema.Attribute.String;
     order_index: Schema.Attribute.Integer;
-    portuguese_translation: Schema.Attribute.String;
+    portuguese_translation: Schema.Attribute.Text;
     publishedAt: Schema.Attribute.DateTime;
     sutra_text: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
@@ -893,12 +893,13 @@ export interface PluginUploadFile extends Struct.CollectionTypeSchema {
     };
   };
   attributes: {
-    alternativeText: Schema.Attribute.String;
-    caption: Schema.Attribute.String;
+    alternativeText: Schema.Attribute.Text;
+    caption: Schema.Attribute.Text;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     ext: Schema.Attribute.String;
+    focalPoint: Schema.Attribute.JSON;
     folder: Schema.Attribute.Relation<'manyToOne', 'plugin::upload.folder'> &
       Schema.Attribute.Private;
     folderPath: Schema.Attribute.String &
@@ -918,7 +919,7 @@ export interface PluginUploadFile extends Struct.CollectionTypeSchema {
       Schema.Attribute.Private;
     mime: Schema.Attribute.String & Schema.Attribute.Required;
     name: Schema.Attribute.String & Schema.Attribute.Required;
-    previewUrl: Schema.Attribute.String;
+    previewUrl: Schema.Attribute.Text;
     provider: Schema.Attribute.String & Schema.Attribute.Required;
     provider_metadata: Schema.Attribute.JSON;
     publishedAt: Schema.Attribute.DateTime;
@@ -927,7 +928,7 @@ export interface PluginUploadFile extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    url: Schema.Attribute.String & Schema.Attribute.Required;
+    url: Schema.Attribute.Text & Schema.Attribute.Required;
     width: Schema.Attribute.Integer;
   };
 }
